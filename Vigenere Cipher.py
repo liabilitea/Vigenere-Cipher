3e2f184dc59d285bfe60bcef74341e00fc0be786
Frances Leigh <fleighbaay@gmail.com>
2023-04-21 14:37:22 +0800

    Define the class

---
+++ Vigenere Cipher.py
@@ -0,0 +1,12 @@
+#Define Vigenere class
+class Vigenere:
+    def __init__(self, message, keyword):
+        self.message = message.upper()
+        self.keyword = keyword.upper()
+
+#Create a method that checks if the length of both the message and keyword are equal
+    
+#Create another method to encrypt the message by appending the keyword characters' values
+
+#Prompt the user to input their message and keyword
+#Print the output
